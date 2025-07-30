6ccead38c92b7048fd068e333dfc6275a525e4af
BRAIS BUSQUE GOÑI <0020250@viewnext.com>
2025-07-30 10:09:35 +0200

    Various7

--- src/main/java/com/sinensia/polloschicharron/integration/repositories/PedidoPLRepository.java
+++ src/main/java/com/sinensia/polloschicharron/integration/repositories/PedidoPLRepository.java
@@ -10,22 +10,28 @@ import com.sinensia.polloschicharron.integration.model.PedidoPL;
 
 public interface PedidoPLRepository extends JpaRepository<PedidoPL, Long> {
 
-	@Query("SELECT new com.sinensia.polloschicharron.business.model.dtos.PedidoDTO1(              "
-		 + "p.id,                                                                          "
-		 + "p.fechaHora,                                                                   "
-		 + "p.establecimiento.nombre,                                                      "
-		 + "UPPER(CONCAT(p.empleado.apellido1,                                             "
-		 + "COALESCE(CONCAT(' ', p.empleado.apellido2), ''),                  "
-		 + "', ',                                                             "
-		 + "p.empleado.nombre)),                                              "
-		 + "CASE WHEN p.estado = EstadoPedidoPL.NUEVO THEN 'NUEVO'                         "
-		 + "WHEN p.estado = EstadoPedidoPL.EN_PROCESO THEN 'EN_PROCESO'"
-		 + "WHEN p.estado = EstadoPedidoPL.PENDIENTE_ENTREGA THEN 'PENDIENTE_ENTREGA'"
-		 + "WHEN p.estado = EstadoPedidoPL.SERVIDO THEN 'SERVIDO'"
-		 + "WHEN p.estado = EstadoPedidoPL.CANCELADO THEN 'CANCELADO'"
-		 + "ELSE NULL"
-		 + "END )"
-		 + "FROM   PedidoPL p   ")
+	@Query("""
+    SELECT new com.sinensia.polloschicharron.business.model.dtos.PedidoDTO1(
+        p.id,
+        p.fechaHora,
+        p.establecimiento.nombre,
+        UPPER(CONCAT(
+            p.empleado.apellido1,
+            COALESCE(CONCAT(' ', p.empleado.apellido2), ''),
+            ', ',
+            p.empleado.nombre
+        )),
+        CASE 
+            WHEN p.estado = com.sinensia.polloschicharron.integration.model.EstadoPedidoPL.NUEVO THEN 'NUEVO'
+            WHEN p.estado = com.sinensia.polloschicharron.integration.model.EstadoPedidoPL.EN_PROCESO THEN 'EN_PROCESO'
+            WHEN p.estado = com.sinensia.polloschicharron.integration.model.EstadoPedidoPL.PENDIENTE_ENTREGA THEN 'PENDIENTE_ENTREGA'
+            WHEN p.estado = com.sinensia.polloschicharron.integration.model.EstadoPedidoPL.SERVIDO THEN 'SERVIDO'
+            WHEN p.estado = com.sinensia.polloschicharron.integration.model.EstadoPedidoPL.CANCELADO THEN 'CANCELADO'
+            ELSE NULL
+        END
+    )
+    FROM PedidoPL p
+    """)
 	List<PedidoDTO1> findDTO1();
 
 } 
